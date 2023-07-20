bb499912d59d7e8f50912577059073ba02b65fa5
AsadbekKazakovDev <asadbekkazakov5300@gmail.com>
2023-07-20 08:25:51 +0500

    solve main

--- basic_function_parameter04.py
+++ basic_function_parameter04.py
@@ -2,3 +2,12 @@
 # Inside the function, calculate the average of all the numbers in the given list.
 # Return the average.
 # Return the average.
+def calculate_average():
+    lst=[85, 90, 92, 88, 95]
+    i=0
+    ans=0
+    while i<len(lst):
+        ans+=lst[i]
+        i+=1
+    return ans//len(lst)
+print(calculate_average())
