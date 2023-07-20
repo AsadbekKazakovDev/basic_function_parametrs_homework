b71a2347eafe0442c21f047785316c5de1a92e88
AsadbekKazakovDev <asadbekkazakov5300@gmail.com>
2023-07-20 08:27:24 +0500

    solve main

--- basic_function_parameter05.py
+++ basic_function_parameter05.py
@@ -1,3 +1,13 @@
 # Create a function named count_vowels that takes a string as a parameter.
 # Inside the function, count the number of vowels (a, e, i, o, u, A, E, I, O, U) in the given string.
 # Return the count of vowels.
+def count_vowels():
+    satr="Hello World!"
+    ans=0
+    i=0
+    while i<len(satr):
+        if satr[i] in ('a','e','i','o','u','A','E','I','O','U'):
+            ans+=1
+        i+=1
+    return ans
+print(count_vowels())
