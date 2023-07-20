d8a1be4c51d8f9b5c1801032e2066325b0cf5c96
AsadbekKazakovDev <asadbekkazakov5300@gmail.com>
2023-07-20 08:21:02 +0500

    solve main

--- basic_function_parameter02.py
+++ basic_function_parameter02.py
@@ -1,3 +1,12 @@
 # Create a function named calculate_sum that takes a list of numbers as a parameter.
 # Inside the function, calculate the sum of all the numbers in the given list.
 # Return the sum.
+def calculate_sum():
+    lst=[2,4,6,8]
+    i=0
+    ans=0
+    while i<len(lst):
+        ans+=lst[i]
+        i+=1
+    return ans
+print(calculate_sum())
